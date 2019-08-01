c95dc9f1f278983a08cdc092bc8f9bdf75deb618
CodeTheCode <assia.ngoran@uvci.edu.ci>
2019-08-01 01:50:05 +0100

    integration awf tips

--- API/awfApi.js
+++ API/awfApi.js
@@ -1,14 +1,1 @@
 //API/awfApi.js
-
-const APIURL = 'http://137.74.116.91'
-
-export function getTipsFromApi(){
-    return fetch(`${APIURL}/allTips`)
-        .then((response) => response.json())
-        .then((responseJson)=> {
-            return responseJson.tips
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-}
--- Components/AwfTipsPage.js
+++ Components/AwfTipsPage.js
@@ -4,12 +4,35 @@ import TipsItem from './Partials/TipsItem'
 import { Button as Buttons } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {Header} from 'react-native-elements'
-import tipsdata from '../Helpers/TipsData'
 
 export default class AwfTipsPage extends React.Component{
 
     constructor(){
         super()
+        this.state = { tips: [], isLoading: true}
+    }
+
+    componentDidMount() {
+        this.getTipsFromApi().then(data => {
+            this.setState({ tips: data.data.tips, isLoading: false})
+        })
+    }
+
+    _displayLoading(){
+        if(this.state.isLoading){
+            return(
+                <View style={styles.loaging_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    }
+    getTipsFromApi(){
+        let url = 'http://137.74.116.91:3334/allTips';
+
+        return fetch(url)
+            .then((response) => response.json())
+            .catch((error) => console.error(error))
     }
 
     render() {
@@ -33,11 +56,12 @@ export default class AwfTipsPage extends React.Component{
                 />
                 <ScrollView>
                     <FlatList
-                        data={tipsdata}
+                        data={this.state.tips}
                         keyExtractor={(item) => item.id.toString()}
                         renderItem={({item}) => <TipsItem tips={item} />}
                         numColumns={2}
                     />
+                    {this._displayLoading()}
                 </ScrollView>
             </View>
         )
@@ -47,5 +71,14 @@ export default class AwfTipsPage extends React.Component{
 const styles = StyleSheet.create({
     container: {
         flex:1,
+    },
+    loaging_container: {
+        position: 'absolute',
+        left: 0,
+        right:0,
+        top:100,
+        bottom:0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
