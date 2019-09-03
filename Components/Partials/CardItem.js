1cbe4b5327e8fc7bd5c97723498aab806b3ed046
CodeTheCode <assia.ngoran@uvci.edu.ci>
2019-09-03 12:35:08 +0100

    recuperatiom actualité image en cours

--- Components/Partials/CardItem.js
+++ Components/Partials/CardItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Stylesheet,} from 'react-native';
+import { View, Text, StyleSheet,Image} from 'react-native';
 import { Card, Button,} from 'react-native-elements'
 
 export default class CardItem extends Component {
@@ -8,21 +8,29 @@ export default class CardItem extends Component {
     }
 
     getImageFromApi (name) {
-        return 'http://137.74.116.91:3334/images/'+ name
+        return 'http://51.68.44.231/images/'+ name
     }
 
     render() {
         const {actualite, goDetail} = this.props;
         return (
-                <Card
-                    title={actualite.title}
-                    image={require('../../assets/actu1.png')}>
-                    <Text style={{marginBottom: 10}}>
-                        {actualite.preview}
+                <View style={styles.container}>
+                    <Text style={{textAlign:'center', fontWeight:'bold'}}>{actualite.title}</Text>
+                    <Image source={{uri: this.getImageFromApi(actualite.photo)}} style={{height:200, width: 300, borderWidth:1}} />
+                    <Text style={{marginBottom: 10}} numberOfLines={4}>
+                    {actualite.article}
                     </Text>
                     <Button onPress={()=>goDetail(actualite.id)}
-                        title="Voir l'article" />
-                </Card>
+                    title="Voir l'article" />
+                </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        padding:5,
+        borderWidth:1,
+        borderColor:'#efefef'
+    }
+})
