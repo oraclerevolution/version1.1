6085da9e49ccba9a27eace701f7fdbc9ec01428a
oraclerevolution <mac.n.drix@gmail.com>
2019-11-09 14:13:01 +0100

    modifs affichages des activites et retrait de quelques footer

--- Components/Partials/AccordionData.js
+++ Components/Partials/AccordionData.js
@@ -17,8 +17,7 @@ const data = [
     {
         id:3,
         title:'Campus AWF',
-        description:`Le Campus AWF est un espace commun de discussion sur les enjeux de la nouvelle Afrique. Ce sont des rencontres de plus de 200 jeunes étudiants et entrepreneurs d’Afrique et d’ailleurs pour réfléchir l’Afrique créative, innovante et entreprenante.
-        Cette rencontre met l’accent sur le vivre ensemble, l’entrepreneuriat, concept important dans la consolidation de la paix. Nos activités visent l’apprentissage, le partage de connaissances et expériences ainsi que l’élaboration des propositions communes.
+        description:`Le Campus AWF est un espace commun de discussion sur les enjeux de la nouvelle Afrique. Ce sont des rencontres de plus de 200 jeunes étudiants et entrepreneurs d’Afrique et d’ailleurs pour réfléchir l’Afrique créative, innovante et entreprenante.Cette rencontre met l’accent sur le vivre ensemble, l’entrepreneuriat, concept important dans la consolidation de la paix. Nos activités visent l’apprentissage, le partage de connaissances et expériences ainsi que l’élaboration des propositions communes.
         `
     },
     {
@@ -29,39 +28,22 @@ const data = [
     {
         id:5,
         title:'Concours (Audiovisuel & Technologique)',
-        description:`Concours adaptés aux réalités africaines et dont le but est d’encourager la création des contenus
-        africains. Doté des prix audiovisuels (documentaire, publicité, fiction, humour, animation originale)
-        et innovations (jeux, applications, sites web), ce concours vise à mettre en lumière le savoir-faire
-        des jeunes producteurs africains.`
+        description:`Concours adaptés aux réalités africaines et dont le but est d’encourager la création des contenus africains. Doté des prix audiovisuels (documentaire, publicité, fiction, humour, animation originale) et innovations (jeux, applications, sites web), ce concours vise à mettre en lumière le savoir-faire des jeunes producteurs africains.`
     },
     {
         id:6,
         title:'Espace incubateurs',
-        description:`Des espaces seront aménagés afin de permettre aux incubateurs de rencontrer des startups ou
-        d’autres porteurs de projets.
-        A cette occasion, le candidat se présente pour convaincre son futur mentor. Par la suite, les deux
-        parties sont gagnantes.`
+        description:`Des espaces seront aménagés afin de permettre aux incubateurs de rencontrer des startups ou d’autres porteurs de projets.A cette occasion, le candidat se présente pour convaincre son futur mentor. Par la suite, les deuxparties sont gagnantes.`
     },
     {
         id:7,
         title:'Hackathon',
-        description:`Le marathon de programmation ou hackathon de AWF est une programmation informatique
-        collaborative qui réunit de développeurs volontaires choisis par l’équipe en charge de projets
-        hackathon en collaboration avec nos partenaires techniques. AWF met ainsi l’écosystème
-        numérique africain et la jeunesse à disposition des organisations afin de leur permettre d’innover.
-        Les organisateurs donnent la chance égale aux candidats et s’assurent de l’adéquation des projets
-        aux réalités socioéconomiques du pays.`
+        description:`Le marathon de programmation ou hackathon de AWF est une programmation informatique collaborative qui réunit de développeurs volontaires choisis par l’équipe en charge de projets hackathon en collaboration avec nos partenaires techniques. AWF met ainsi l’écosystème numérique africain et la jeunesse à disposition des organisations afin de leur permettre d’innover.Les organisateurs donnent la chance égale aux candidats et s’assurent de l’adéquation des projets aux réalités socioéconomiques du pays.`
     },
     {
         id:8,
         title:'Programme Jeune Public',
-        description:`Afin d’être au plus près du jeune public AWF et favoriser plus d’interaction, un espace à double
-        fonction : espace d’information sur des thématiques divers et espace culturel (animation auprès du
-        public, manifestations culturelles, écran géant de diffusion, danse, etc.) sera dédié au Programme
-        Jeune Public. Elle servira également à la présentation des produits et services de entreprises
-        partenaires (démo, présentation, jeux interactifs, etc.) Caractérisé par un public jeune et la diversité
-        des champs d’activité, cette espace a un éventail des tâches qui le rend flexible et adaptable aux
-        besoins des clients.`
+        description:`Afin d’être au plus près du jeune public AWF et favoriser plus d’interaction, un espace à double fonction : espace d’information sur des thématiques divers et espace culturel (animation auprès du public, manifestations culturelles, écran géant de diffusion, danse, etc.) sera dédié au Programme Jeune Public. Elle servira également à la présentation des produits et services de entreprises partenaires (démo, présentation, jeux interactifs, etc.) Caractérisé par un public jeune et la diversité des champs d’activité, cette espace a un éventail des tâches qui le rend flexible et adaptable aux besoins des clients.`
     },
 ]
 
